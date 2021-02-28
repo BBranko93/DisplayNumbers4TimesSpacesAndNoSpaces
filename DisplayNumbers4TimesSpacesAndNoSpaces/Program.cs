018223af33cb291cbfcce51fec81c6b4dfacec25
BBranko93 <bane_b80@outlook.com>
2021-02-27 20:41:05 -0700

    Add project files.

---
+++ DisplayNumbers4TimesSpacesAndNoSpaces/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DisplayNumbers4TimesSpacesAndNoSpaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, i;
+            Console.WriteLine("Please enter a number : ");
+            num1 = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < 4; i++)
+            {
+                Console.Write("{0} ", num1);
+            }
+            Console.WriteLine();
+            for (i = 0; i < 4; i++)
+            {
+                Console.Write("{0}",num1);
+            }
+        }
+    }
+}
